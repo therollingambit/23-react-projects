a169c29ee7fc649eb2d8f25e70f93c4be6a25601
Ahmad Enayathullah <ahmad.enayathullah@fdmgroup.com>
2021-05-23 21:45:36 +0800

    added color generator project

---
+++ color-generator/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      setError(false);
+    } catch (e) {
+      setError(true)
+      console.log(e)
+    }
+  }
+
+  return (
+    <>
+    <section className="container">
+      <h3>Color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          value={color} 
+          onChange={e => setColor(e.target.value)} 
+          placeholder='#f15025' 
+          className={error ? 'error' : ''}
+        />
+        <button className="btn" type='submit'>submit</button>
+      </form>
+    </section>
+    <section className="colors">
+      {
+        list.map((color, index) => (
+          <SingleColor key={index} {...color} index={index} hexColor={color.hex} />
+        ))
+      }
+    </section>
+    </>
+  );
+}
+
+export default App
